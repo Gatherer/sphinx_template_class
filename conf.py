97eaea687ba616565870d3e504508ab9dd9c3999
Gatherer <gatherer.thomas@gmx.de>
2023-01-13 21:45:12 +0100

    Add latex.tex_t test

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -278,7 +278,7 @@ latex_documents = [
     'test.tex',
     u'test Documentation',
     u'test',
-    'mymanual'),
+    'manual'),
 ]
 
 latex_additional_files = [
